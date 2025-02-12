903d7555095cb591e1606c7c125a62c8b6af3bb5
bharathijana1 <160519295+bharathijana1@users.noreply.github.com>
2025-02-12 10:53:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 // import './App.css'
 import AdviceApp from './Components/AdviceApp'
+import AdviceapplogoImg form '../Components/img/advice.png" alt="advice app';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
     <div className='bg-rose-200 flex justify-start items-center '>
-    <img src="../src/Components/img/advice.png" alt="advice app"  className='bg-rose-200 m-3' width="80px" height="80px" />
+    <img src={AdviceapplogoImg} alt="advice app"  className='bg-rose-200 m-3' width="80px" height="80px" />
     <h1 className='font-bold text-3xl text-center p-2  py-6 text-rose-950'>Advicer App</h1>
     </div>
     
